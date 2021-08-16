b44cfdda23506e03c3fcdca8e7457395386e152d
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-08-16 16:02:28 -0300

    buscando proposta por id

--- src/main/java/com/zupedu/gabriel/microservico/resouces/PropostaResource.java
+++ src/main/java/com/zupedu/gabriel/microservico/resouces/PropostaResource.java
@@ -1,11 +1,14 @@
 package com.zupedu.gabriel.microservico.resouces;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.zupedu.gabriel.microservico.dtos.PropostaDTO;
+import com.zupedu.gabriel.microservico.entities.Proposta;
 import com.zupedu.gabriel.microservico.entities.enums.Status;
 import com.zupedu.gabriel.microservico.repositories.PropostaRepository;
 import com.zupedu.gabriel.microservico.resouces.exceptions.utils.DataIntegrityException;
+import com.zupedu.gabriel.microservico.resouces.exceptions.utils.ObjectNotFoundException;
 import com.zupedu.gabriel.microservico.resouces.utils.StatusClienteResponse;
 
 import feign.FeignException;
@@ -46,6 +51,16 @@ public class PropostaResource {
 			propostaRepository.save(entityToSave);
 			throw new DataIntegrityException("Documento com restrição!");
 		}
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PropostaDTO> findById(@PathVariable Long id) {
+		Optional<Proposta> obj = propostaRepository.findById(id);
+		var objDto = new PropostaDTO(obj.get());
+		obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Proposta.class.getName()));
+		return ResponseEntity.ok().body(objDto);
 
 	}
 
--- src/main/java/com/zupedu/gabriel/microservico/resouces/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/zupedu/gabriel/microservico/resouces/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.zupedu.gabriel.microservico.resouces.exceptions;
 
+import java.util.NoSuchElementException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.zupedu.gabriel.microservico.resouces.exceptions.utils.DataIntegrityException;
+import com.zupedu.gabriel.microservico.resouces.exceptions.utils.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -24,6 +27,16 @@ public class ResourceExceptionHandler {
 		}
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<StandardError> notFound(NoSuchElementException e, HttpServletRequest request) {
+
+		ValidatorError err = new ValidatorError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Erro de validação");		
+			err.addError("Id", "Objeto não encontrado!");
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
 
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
@@ -31,6 +44,14 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(),
 				"Proposta para esse cliente já existe!");
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
+	}
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+				"Não encontrado");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
 }
